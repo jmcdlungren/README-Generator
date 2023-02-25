af6bdef7ce8afd62535c29ceeaf7108ae57b534a
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-24 22:05:47 -0700

    removed Badges text - will have info added in later

--- index.js
+++ index.js
@@ -31,9 +31,7 @@ ${usage}
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
 ## Questions and Contributing
 
