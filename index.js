e35a8852e7f1d5239ba09155deea2b630d7110eb
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-26 17:03:24 -0700

    added in license function

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const markdown = require('./Utilities/generateMarkdown.js');
 
-const generateREADME = ({ title, description, installation, usage, email, github, tests }) =>
+
+
+function renderLicense(license) {
+    if (license == "The Hippocratic License") {
+        console.log("The Hippocratic License is chosen")
+        return "[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)"
+    }
+    else if (license == "ISC") {
+        console.log("ISC is chosen")
+        return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+    }
+    else if (license == "MIT") {
+        console.log("MIT is chosen")
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    }
+    else if (license == "Mozilla") {
+        console.log("Mozilla is chosen")
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    }
+    else if (license == "Unlicense") {
+        console.log("Unlicense is chosen")
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    }
+    else if (license == "None") {
+        return ""
+    }
+}
+
+const generateREADME = ({ title, description, installation, usage, license, email, github, tests }) =>
     `# ${title}
 
 ## Description
@@ -26,7 +53,9 @@ ${installation}
 
 ${usage}
 
-${markdown.renderLicenseSection()}
+## License
+
+${renderLicense(license)}
 
 ## Questions and Contributing
 
@@ -37,6 +66,8 @@ If you are interested in contributing, please follow the guidelines outlined wit
 ## Tests
 
 ${tests}`
+
+
 
 inquirer
     .prompt([
@@ -61,6 +92,12 @@ inquirer
             message: 'What are some examples and instructions for use?',
         },
         {
+            type: 'list',
+            message: 'Please choose the applicable license(s), if any.',
+            name: 'license',
+            choices: ['The Hippocratic License', 'ISC', 'MIT', 'Mozilla', 'Unlicense', 'None'],
+        },
+        {
             type: 'input',
             name: 'email',
             message: 'Enter your email address.',
@@ -78,19 +115,10 @@ inquirer
     ])
     .then((answers) => {
         const READMEcontent = generateREADME(answers);
-
+        console.log(answers)
         fs.writeFile('./Generated README/README.md', READMEcontent, (err) =>
             err ? console.log(err) : console.log('Successfully created README.md!')
         );
     });
 
 
-
-
-// TODO: Create a function to initialize app
-function init() {
-
-}
-
-// Function call to initialize app
-init();
